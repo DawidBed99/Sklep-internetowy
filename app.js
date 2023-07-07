84966c56a92f715ef26ea3912493fca3d19d9e7c
Dawid Bednarz <160144@stud.prz.edu.pl>
2023-07-07 13:41:20 +0200

    Basket

--- app.js
+++ app.js
@@ -5,12 +5,39 @@ const SAMSUNG = document.querySelectorAll(".Samsung");
 const container = document.querySelector(".container");
 var basketnumber = document.querySelector(".basket_number");
 const addBasket = document.querySelectorAll(".add_to_basket");
+const number_cont = document.querySelector(".number_cont");
+const basket = document.querySelector(".basket");
+const itemsBasket = document.querySelector(".itemsinbasket");
+const clearBasket = document.querySelector(".clear_text");
+const itemsContainer = document.querySelector(".items_container");
 var count = 0;
+clearBasket.addEventListener("click", () => {
+  var childElements = document.querySelector(".items_container");
+  var delChild = childElements.lastChild;
+  while (delChild) {
+    childElements.removeChild(delChild);
+    delChild = childElements.lastChild;
+  }
+  count = 0;
+
+  number_cont.classList.add("hidden");
+});
+
+basket.addEventListener("mouseover", () => {
+  itemsBasket.classList.remove("hidden");
+});
+basket.addEventListener("mouseout", () => {
+  itemsBasket.classList.add("hidden");
+});
+
 addBasket.forEach((add_to_basket) => {
   add_to_basket.addEventListener("click", function () {
     count++;
-    console.log(count);
     basketnumber.innerHTML = count;
+    number_cont.classList.remove("hidden");
+    var item_crea = document.createElement("div");
+    item_crea.className = "item_added";
+    itemsContainer.appendChild(item_crea);
   });
 });
 
