b612c02e76e58bacf37c1b06889e0c828fcbb87b
Dawid Bednarz <160144@stud.prz.edu.pl>
2023-07-08 14:38:34 +0200

    Usprawniony koszyk

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const MENU = document.querySelectorAll(".choice");
-const IPHONE = document.querySelectorAll(".iphone");
-const HUAWEI = document.querySelectorAll(".Huawei");
-const SAMSUNG = document.querySelectorAll(".Samsung");
+const IPHONE = document.querySelectorAll("#iphone");
+const HUAWEI = document.querySelectorAll("#Huawei");
+const SAMSUNG = document.querySelectorAll("#Samsung");
 const container = document.querySelector(".container");
 var basketnumber = document.querySelector(".basket_number");
 const addBasket = document.querySelectorAll(".add_to_basket");
@@ -10,7 +10,12 @@ const basket = document.querySelector(".basket");
 const itemsBasket = document.querySelector(".itemsinbasket");
 const clearBasket = document.querySelector(".clear_text");
 const itemsContainer = document.querySelector(".items_container");
+const products = document.querySelectorAll(".mphone");
+const image = document.querySelectorAll(".phone");
 var count = 0;
+
+//----------
+
 clearBasket.addEventListener("click", () => {
   var childElements = document.querySelector(".items_container");
   var delChild = childElements.lastChild;
@@ -23,11 +28,18 @@ clearBasket.addEventListener("click", () => {
   number_cont.classList.add("hidden");
 });
 
-basket.addEventListener("mouseover", () => {
-  itemsBasket.classList.remove("hidden");
-});
-basket.addEventListener("mouseout", () => {
-  itemsBasket.classList.add("hidden");
+// basket.addEventListener("mouseover", () => {
+//   itemsBasket.classList.remove("hidden");
+// });
+// basket.addEventListener("mouseout", () => {
+//   itemsBasket.classList.add("hidden");
+// });
+basket.addEventListener("click", () => {
+  if (itemsBasket.classList.contains("hidden")) {
+    itemsBasket.classList.remove("hidden");
+  } else {
+    itemsBasket.classList.add("hidden");
+  }
 });
 
 addBasket.forEach((add_to_basket) => {
@@ -38,6 +50,29 @@ addBasket.forEach((add_to_basket) => {
     var item_crea = document.createElement("div");
     item_crea.className = "item_added";
     itemsContainer.appendChild(item_crea);
+    var smallphone = document.createElement("img");
+    smallphone.className = "small_phone";
+    var img_src = add_to_basket.parentElement.parentElement
+      .querySelector(".phone")
+      .getAttribute("src");
+    smallphone.setAttribute("src", img_src);
+    item_crea.appendChild(smallphone);
+    var item_name = document.createElement("p");
+    item_name.className = "phone_name";
+    item_name.innerHTML =
+      add_to_basket.parentElement.parentElement.querySelector(
+        ".item_name"
+      ).textContent;
+
+    item_crea.appendChild(item_name);
+    var item_price = document.createElement("p");
+    item_price.className = "phone_price";
+
+    item_price.textContent =
+      "Cena: " +
+      add_to_basket.parentElement.querySelector(".price_sale").textContent;
+
+    item_crea.appendChild(item_price);
   });
 });
 
